77ed57c4f9a3baddd48be43c1bfd4dc8a610835b
christinamartinez <christina.martinez@faithlife.com>
2024-09-30 11:34:49 -0700

    Add more bad code...

--- src/main.ts
+++ src/main.ts
@@ -190,6 +190,26 @@ function redundantFunction2(
   });
 }
 
+function redundantFunction3(
+  file: File,
+  chunk: Chunk,
+  aiResponses: Array<{
+    lineNumber: string;
+    reviewComment: string;
+  }>
+): Array<{ body: string; path: string; line: number }> {
+  return aiResponses.flatMap((aiResponse) => {
+    if (!file.to) {
+      return [];
+    }
+    return {
+      body: aiResponse.reviewComment,
+      path: file.to,
+      line: Number(aiResponse.lineNumber),
+    };
+  });
+}
+
 function createComment(
   file: File,
   chunk: Chunk,
